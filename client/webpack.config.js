0a74e4300d8e8d9259b815b748b219e2e23aa4de
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-09-20 15:44:38 +1000

    Add and configure workbox plugins for a service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,59 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: "development",
+		// Entry point for files.
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+		},
+		// Output for our bundles.
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [
+			// Webpack plugin that generates our html file and injects our bundles.
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "Just Another Text Editor",
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			// Injects our custom service worker.
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+
+			// Creates a manifest.json file.
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Just Another Text Editor",
+				short_name: "jate",
+				description: "Create notes with or without an internet connection!",
+				background_color: "#272822",
+				theme_color: "#272822",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
+		],
+
+		module: {
+			rules: [],
+		},
+	};
 };
