00dd4c66402d5320f406ad80186879cf495a2c9f
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-09-20 15:25:57 +1000

    Add logic to PUT method to update content in jateDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,26 @@ const initdb = async () =>
 		},
 	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+// Accepts some content and add it to the database.
+export const putDb = async (content) => {
+	console.log("PUT to the database");
+
+	// Create a connection to the database and version we want to use.
+	const jateDb = await openDB("jate", 1);
+
+	// Create a new transaction and specify the database and data privileges.
+	const tx = jateDb.transaction("jate", "readwrite");
+
+	// Open up the desired object store.
+	const store = tx.objectStore("jate");
+
+	// Use the .put() method to update data in the database.
+	const request = store.put({ content: content });
+
+	// Get confirmation of the request.
+	const result = await request;
+	console.log("🚀 - data saved to the database", result);
+};
 
 // Gets all the content from the database.
 export const getDb = async () => {
@@ -33,7 +51,7 @@ export const getDb = async () => {
 
 	// Get confirmation of the request.
 	const result = await request;
-	console.log("result.value", result);
+	console.log("🚀 - data retrieved from the database", result);
 	return result;
 };
 
