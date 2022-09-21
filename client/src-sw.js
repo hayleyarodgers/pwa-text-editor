a3712be1eeb64154be4eb88a5606dab9ee2f75e5
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-09-21 19:57:35 +1000

    Add missing StaleWhileRevalidate class from require statement

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -39,5 +39,3 @@ registerRoute(
 		],
 	})
 );
-
-offlineFallback();
